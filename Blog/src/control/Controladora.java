97756a54c541bcbb4f01ee83ea1b7f75951d0261
NeyB19 <160780976+NeyB19@users.noreply.github.com>
2026-04-30 02:55:50 -0600

    Ejemplos / Main
    Los ejemplos por defecto y parte del main.

--- Blog/src/control/Controladora.java
+++ Blog/src/control/Controladora.java
@@ -9,6 +9,22 @@ public class Controladora {
 
     public Controladora() {
         blogs = new TreeMap<Integer, Blog>();
+        
+        // Blogs por defecto
+        
+        crearBlog("Mundo Canino", "Todo sobre razas de perros"); 
+        crearBlog("Jardín Floral", "Guía de flores y cuidados");
+
+        try {
+            crearPublicacion(1, "El Golden Retriever", "Es un perro muy amigable.", "Administrador");
+            crearPublicacion(1, "Cuidado del Bulldog", "Necesita limpieza de pliegues.", "Veterinario"); 
+
+            agregarComentario(1, 0, "ana12l@gmail.com", "192.168.0.1", "Me encantan los Goldens");
+            agregarComentario(1, 0, "andyb@gmail.com", "192.168.0.2", "Son los mejores perros");
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     public void crearBlog(String nombre, String descripcion) {
--- Blog/src/interfaz/EjemploBlog.java
+++ Blog/src/interfaz/EjemploBlog.java
@@ -1,10 +1,42 @@
 package interfaz;
 
+import java.util.Scanner;
+import control.Controladora;
+
 public class EjemploBlog {
+    private static Controladora control = new Controladora();
+    private static Scanner teclado = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        int opcion = 0;
+        while (opcion != 4) {
+            try {
+                System.out.println("\n____ MENÚ PRINCIPAL ____");
+                System.out.println("1. Crear blog nuevo");
+                System.out.println("2. Trabajar con blog");
+                System.out.println("3. Borrar blog");
+                System.out.println("4. Salir");
+                System.out.print("Seleccione: ");
+                opcion = Integer.parseInt(teclado.nextLine());
 
-	}
-
+                if (opcion == 1) {
+                    System.out.print("Nombre: "); String nombre = teclado.nextLine();
+                    System.out.print("Descripción: "); String descrip = teclado.nextLine();
+                    control.crearBlog(nombre, descrip);
+                    System.out.println("Blog creado...");
+                } 
+                else if (opcion == 2) {
+                    menuBlogs();
+                } 
+                else if (opcion == 3) {
+                    System.out.print("Código del blog a borrar: ");
+                    int codigoBlog = Integer.parseInt(teclado.nextLine());
+                    control.borrarBlog(codigoBlog);
+                    System.out.println("Blog eliminado");
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
 }
